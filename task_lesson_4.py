602618d4814eb86f6c67116556e3939e329f4a22
jhook-py <chirca.ion.fl@gmail.com>
2019-11-08 01:50:40 +0200

    exercitii lectia 4 din 07.11.2019

---
+++ task_lesson_4.py
@@ -0,0 +1,76 @@
+"""
+Scrieti un program care detecteaza daca un fisier are extensia py.
+Daca extensia este diferita de py printati-o pe ecran.
+Daca fisierul nu are extensie printati pe ecaran un mesaj de avrtizare.
+(Folositi functiile input, split(), conditionale)
+"""
+
+#a = input("Introduce numele de fisier >>> ")
+#
+#if a.find(".") == -1:
+#    print("Atentie, fisierul nu are extensie!")
+#elif a.split("."):
+#    if a[-2:] == "py":
+#        print(a)
+#        print("True - fisierul are extensia .py")
+#    else:
+#        print("Fisierul nu are extensia .py dar: .{}".format(a[-2:]))
+#else:
+#    None
+
+###############################################################################
+###############################################################################
+
+
+"""
+Scrieti un program determina daca un text este palindrom (caiac, capac,minim)
+"""
+
+#a = input("Introduce cuvantul >>> ")
+#b = a[::-1]
+#if a == b:
+#    print("Cuvantul introdus este un palindrom")
+#else:
+#    print("Cuvantul introdus nu este un palindrom")
+
+###############################################################################
+###############################################################################
+
+"""
+Calculati suma numerelor pare mai mici decit 100.
+"""
+
+#print(sum(num for num in range(2, 100, 2)))
+
+###############################################################################
+###############################################################################
+
+""" Scrieti un program care converteste temperatura din grade celsius in
+farenheit si invers (formula: c/5 = f-32/9)
+c = 5 * (f - 32/9)
+f = c/5 + 32/9
+"""
+
+
+
+#def convert():
+#    a = input("Introduceti valoarea temperaturii >>> ")
+#    if a.isdigit():
+#        b = input("Introduceti scara, `C` sau `F` >>> ")
+#        if (b == "C" or b == "c"):
+#            print("Temperatura este %sF" % round((int(a)/5 + 32/9), 2))
+#        elif (b == "F" or b == "f"):
+#            print("Temperatura este %sC" % round((5 * (int(a) - 32/9)), 2))
+#        else:
+#            print("Scara introdusa este gresita")
+#            convert()
+#    else:
+#        print("Valoarea introdusa trebuie sa fie un numar intreg")
+#        convert()
+#
+#convert()
+
+
+
+###############################################################################
+###############################################################################
