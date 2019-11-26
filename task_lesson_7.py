d4d4b33072bd9d78a0e00da9688bee1cd9862a76
jhook-py <chirca.ion.fl@gmail.com>
2019-11-26 21:47:05 +0200

    changes

--- task_lesson_7.py
+++ task_lesson_7.py
@@ -2,13 +2,19 @@
 functiile reduce / filter"""
 # from functools import reduce
 # myList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
-#
 # newList = reduce(lambda x, y: x + y, (filter(lambda x: x % 2 == 0, myList)))
-#
 # print(newList)
 
 """Scrieti un functie generator care sa lucreze ca functia enumerate (yield)"""
 
+# import string
+# numbers = [num for num in range(30)]
+# letters = [char for char in string.ascii_lowercase]
+# def zipGenerator():
+#     yield list(zip(numbers, letters))
+#
+# for pair in zipGenerator():
+#     print(pair)
 
 """Scrieti un program care transforma o lista cu elemente compuse intr-o lista
 simpla ( [1, [2, 3, [4, 5]]] -> [1,2,3,4,5] )"""
@@ -24,13 +30,34 @@ simpla ( [1, [2, 3, [4, 5]]] -> [1,2,3,4,5] )"""
 # myList.append(next(c))
 # print(myList)
 
+# myList = [[1], [2, 3], [4, 5]]
+# x = sum(myList, [])
+# print(x)
+
+# from functools import reduce
+# myList = [[1], [2, 3], [4, 5]]
+# flatList = reduce(lambda z, y :z + y, myList)
+# print(flatList)
+
+import itertools
+list_of_lists = [1, [2, 3, [4, 5]]]
+if item isdigit) in list_of_lists
+chain = list(itertools.chain(*list_of_lists))
+print(chain)
+
 """Scrieti o functie care primeste 2 parametri, 1 functie si o lista, rezultatul
 returnat trebuie sa fie lista modificata de fuctie:
 
 In: def my_fync(lambda x: x*x, [1, 2, [3, 4, [5]]])
 Out: [1, 4, [9, 16, [25]]]"""
 
-
-
-a = (lambda x: x*5)([1, 2, [3, 4, [5]]])
-print(a)
+# myFunc = lambda x: x * x
+# myList = [num for num in range(10)]
+# def listFunc(*args):
+#     newList = []
+#     for item in myList:
+#         item = myFunc(int(item))
+#         newList.append(item)
+#     return newList
+#
+# print(listFunc(myFunc, myList))
